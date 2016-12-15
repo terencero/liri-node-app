e49f1d6292d8f3c6005dd8f70f04273810bd7895
Terence Ro <terencero@dhcp-10-102-236-249.wireless.northwestern.private>
2016-12-14 18:23:16 -0600

    add twitter call

--- liri.js
+++ liri.js
@@ -1,13 +1,24 @@
 var keys = require('./keys.js');
 
+var twitter = require('twitter');
+
 var inquirer = require('inquirer');
 
+var http = require('http');
+
 // Create prompt with list for the command choices
+
+var client = new twitter ({
+	consumer_key: twitterKeys.consumer_key,
+	consumer_secret: twitterKeys.consumer_secret,
+	access_token_key: twitterKeys.access_token_key,
+	access_token_secret: twitterKeys.access_token_secret
+});
 
 inquirer.prompt([
 
 	{
-		type: 'list',
+		type: 'checkbox',
 		message: 'Choose a request.',
 		choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'],
 		name: 'commands'
@@ -16,6 +27,12 @@ inquirer.prompt([
 ]).then(function (user){
 	if(user.commands === 'my-tweets') {
 		console.log('Ok ' + user.commands);
+		function getTweets(){
+			return http.get({
+				host: 'api.twitter.com/oauth2/token',
+				path: ''
+			});
+		}
 		
 	} else if (user.commands === 'spotify-this-song') {
 		console.log('Ok ' + user.commands);
@@ -30,6 +47,38 @@ inquirer.prompt([
 			console.log('You chose ' + user.song);
 		});
 		
+	} else if (user.commands === 'movie-this') {
+		console.log('Ok ' + user.commands);
+		inquirer.prompt([
+
+			{	
+				type: 'input',
+				message: 'You chose movie-this. Now name a movie.',
+				name: 'movie'
+			}
+
+		]).then(function (user){
+			console.log('You chose ' + user.movie);
+		});
+	} else if (user.commands === 'do-what-it-says') {
+		console.log('Ok ' + user.commands);
+
+		var fs = require(fs);
+		fs.readFile('random.txt', 'utf8', function(err, data){
+			var result = data.split(',').forEach(function (item){
+				console.log(item);
+			});
+		});
+		inquirer.prompt([
+
+			{	
+				type: 'input',
+				default: result[]
+			}
+
+			]).then(function (user){
+
+			});
 	}
 });
 
