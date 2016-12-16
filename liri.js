c73a3467fd92f00e1df4f4b8121b6a164c8ba867
Terence Ro <terencero@dhcp-10-105-142-67.wireless.northwestern.private>
2016-12-16 10:00:43 -0600

    add code to tweet most recent tweets

--- liri.js
+++ liri.js
@@ -6,19 +6,23 @@ var inquirer = require('inquirer');
 
 var http = require('http');
 
+var doWhatItSays = process.argv[2];
+
 // Create prompt with list for the command choices
 
 var client = new twitter ({
-	consumer_key: twitterKeys.consumer_key,
-	consumer_secret: twitterKeys.consumer_secret,
-	access_token_key: twitterKeys.access_token_key,
-	access_token_secret: twitterKeys.access_token_secret
+	consumer_key: keys.twitterKeys.consumer_key,
+	consumer_secret: keys.twitterKeys.consumer_secret,
+	access_token_key: keys.twitterKeys.access_token_key,
+	access_token_secret: keys.twitterKeys.access_token_secret
 });
+
+// var params = {sreen_name: 'nodejs'};
 
 inquirer.prompt([
 
 	{
-		type: 'checkbox',
+		type: 'list',
 		message: 'Choose a request.',
 		choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'],
 		name: 'commands'
@@ -27,12 +31,19 @@ inquirer.prompt([
 ]).then(function (user){
 	if(user.commands === 'my-tweets') {
 		console.log('Ok ' + user.commands);
-		function getTweets(){
-			return http.get({
-				host: 'api.twitter.com/oauth2/token',
-				path: ''
-			});
-		}
+		client.get('statuses/user_timeline', {count: 20, screen_name: '@tro1109'}, function(error, tweets, response){
+			if(error) throw error;
+			// console.log(tweets);
+			// console.log(response);
+			var tweetsResults = [];
+			for (var i = 0; i < tweets.length; i++) {
+
+				tweetsResults.push(tweets[i].text + ' --created on-- ' + tweets[i].created_at);
+				tweetsResults.push('_________________________________________________________');
+
+			}
+			console.log(JSON.stringify(tweetsResults, null, 2));
+		});
 		
 	} else if (user.commands === 'spotify-this-song') {
 		console.log('Ok ' + user.commands);
@@ -69,16 +80,7 @@ inquirer.prompt([
 				console.log(item);
 			});
 		});
-		inquirer.prompt([
-
-			{	
-				type: 'input',
-				default: result[]
-			}
-
-			]).then(function (user){
-
-			});
+		
 	}
 });
 
